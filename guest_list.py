7d9993fe7b5f98bfd4a166797b4478e38738c67d
Erik Olvera <erikolvera44@gmail.com>
2025-09-07 23:08:22 -0500

    made a menu for user to choose

---
+++ guest_list.py
@@ -0,0 +1,14 @@
+erik_list = []
+melanie_list = []
+
+def show_menu():
+    print("\n--- Wedding Guest List ---")
+    print("1. Add guest")
+    print("2. Remove guest")
+    print("3. View guest lists")
+    print("4. See total count")
+    print("5. Exit")
+
+while True:
+    show_menu()
+    choice = input("Choose choices 1-5: ")
