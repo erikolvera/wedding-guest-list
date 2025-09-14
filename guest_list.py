b7a4adbc7263d1a2866449f6323a9bc76bfc683d
Erik Olvera <erikolvera44@gmail.com>
2025-09-13 20:14:19 -0500

    added match cases to the program. added some logic to case 1 and 4

--- guest_list.py
+++ guest_list.py
@@ -1,5 +1,6 @@
 husband_list = []
 wife_list = []
+combined_lists = []
 
 def show_menu():
     print("\n--- Wedding Guest List ---")
@@ -9,6 +10,38 @@ def show_menu():
     print("4. See total count")
     print("5. Exit")
 
+
+#case 1
+def add_guest():
+    print("Would you like to add to the husband's or wife's list?")
+    husband_or_wife = input()
+    
+#case 2
+def remove_guest():
+    pass
+#case 3
+def view_guest_names():
+    pass
+
+# case 4
+def see_total_count():
+    print(len(combined_lists)) # print the total of guests from both husband and wife guest list
+
+# case 5 just exits the while loop
+
 while True:
     show_menu()
     choice = input("Choose choices 1-5: ")
+
+    match choice:
+        case "1":
+            add_guest()
+        case "2":
+            break
+        case "3":
+            break
+        case "4":
+            see_total_count()
+        case "5":
+            break
+
